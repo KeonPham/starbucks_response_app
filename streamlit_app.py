1dfdb62cd8807d05942be128e421c75d7fd0e273
Khoa Pham <85378140+KeonPham@users.noreply.github.com>
2024-11-09 23:32:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,30 +8,37 @@ st.write("""-- This app predicts whether a customer will respond to a specific o
 """)
 st.write(':point_left: (click arrow sign to hide or unhide the form) :green[Please fill out the input fields below for prediction.] :sunglasses:')
 
-
 # Collect user input features into a DataFrame
 def user_input_features():
-    amount = st.number_input('Total Transaction Amount ($): ')
-    time = st.number_input('Time (minutes): ')
-    age = st.number_input('Age of Customer: ')
-    income = st.number_input('Annual Income ($): ')
-    difficulty = st.number_input('Offer Difficulty (0, 5, 7, 10, 20): ')
-    duration = st.number_input('Offer Duration (days) - (3, 4, 5, 7, 10): ')
-    offer_type_bogo = st.slider('Proportion of Offer Type - BOGO (0-1 scale): ', 0.0, 1.0, 0.0)
-    offer_type_discount = st.slider('Proportion of Offer Type - Discount (0-1 scale): ', 0.0, 1.0, 0.0)
-    offer_type_informational = st.slider('Proportion of Offer Type - Informational (0-1 scale): ', 0.0, 1.0, 0.0)
-    web = st.number_input('Number of Interactions via Web: ')
-    email = st.number_input('Number of Interactions via Email: ')
-    mobile = st.number_input('Number of Interactions via Mobile: ')
-    social = st.number_input('Number of Interactions via Social Media: ')
-    membership_duration = st.number_input('Membership Duration (days): ')
-    difficulty_income_interaction = st.number_input('Difficulty-Income Interaction: ')
-    gender_F = st.selectbox('Gender (Female=1, else=0)', (0, 1))
-    gender_M = st.selectbox('Gender (Male=1, else=0)', (0, 1))
-    gender_O = st.selectbox('Gender (Other=1, else=0)', (0, 1))
-    year_become_member = st.number_input('Year Became a Member: ', min_value=2015, max_value=2024, step=1)
-    month_become_member = st.number_input('Month Became a Member (1-12): ', min_value=1, max_value=12, step=1)
-    date_become_member = st.number_input('Date Became a Member (1-31): ', min_value=1, max_value=31, step=1)
+    # Create columns for input fields to be side by side
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        amount = st.number_input('Total Transaction Amount ($): ')
+        time = st.number_input('Time (minutes): ')
+        age = st.number_input('Age of Customer: ')
+        income = st.number_input('Annual Income ($): ')
+        difficulty = st.number_input('Offer Difficulty (0, 5, 7, 10, 20): ')
+        duration = st.number_input('Offer Duration (days) - (3, 4, 5, 7, 10): ')
+        
+    with col2:
+        offer_type_bogo = st.slider('Proportion of Offer Type - BOGO (0-1 scale): ', 0.0, 1.0, 0.0)
+        offer_type_discount = st.slider('Proportion of Offer Type - Discount (0-1 scale): ', 0.0, 1.0, 0.0)
+        offer_type_informational = st.slider('Proportion of Offer Type - Informational (0-1 scale): ', 0.0, 1.0, 0.0)
+        web = st.number_input('Number of Interactions via Web: ')
+        email = st.number_input('Number of Interactions via Email: ')
+        mobile = st.number_input('Number of Interactions via Mobile: ')
+        
+    with col3:
+        social = st.number_input('Number of Interactions via Social Media: ')
+        membership_duration = st.number_input('Membership Duration (days): ')
+        difficulty_income_interaction = st.number_input('Difficulty-Income Interaction: ')
+        gender_F = st.selectbox('Gender (Female=1, else=0)', (0, 1))
+        gender_M = st.selectbox('Gender (Male=1, else=0)', (0, 1))
+        gender_O = st.selectbox('Gender (Other=1, else=0)', (0, 1))
+        year_become_member = st.number_input('Year Became a Member: ', min_value=2015, max_value=2024, step=1)
+        month_become_member = st.number_input('Month Became a Member (1-12): ', min_value=1, max_value=12, step=1)
+        date_become_member = st.number_input('Date Became a Member (1-31): ', min_value=1, max_value=31, step=1)
 
     data = {
         'amount': amount, 'time': time, 'age': age, 'income': income, 'difficulty': difficulty, 'duration': duration,
